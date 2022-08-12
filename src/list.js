d9f69be2de33a17e90763f25949b82633a8d2211
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-08-12 18:55:24 +0430

    Clear commented part

--- src/list.js
+++ src/list.js
@@ -1,9 +1,6 @@
 import 'lodash';
 
 import dots from '../assets/images/dots.png';
-// import save from '../assets/images/save.jpeg';
-// import removeIcon from '../assets/images/delete.png';
-
 class DoList {
   constructor(description, completed, index) {
     this.description = description;
@@ -69,55 +66,9 @@ const displayTask = () => {
       order.appendChild(input);
       order.appendChild(editIcon);
     });
-    // document.querySelectorAll('.btn-edit').forEach((e) => {
-    //   e.addEventListener('click');
-    // });
   } else {
     document.querySelector('todo-list').innerHTML = '';
   }
 };
-
-// const editTask = (e) => {
-//   const editItem = e.target;
-//   const editBtn = document.getElementById(`${editItem.id}`);
-//   const itemId = document.querySelector(`#do-activity-${editItem.id}`);
-//   itemId.removeAttribute('readonly');
-//   itemId.focus();
-
-//   editBtn.style.display = 'none';
-//   itemId.style.outlineColor = 'gray';
-
-//   const taskList = document.querySelector(`#tasks-${editItem.id}`);
-//   taskList.classList.add('active');
-
-//   const singleTask = document.querySelector(`#task-${editItem.id}`);
-
-//   const saveChange = document.createElement('img');
-//   saveChange.setAttribute('src', save);
-//   saveChange.classList.add(`save-${editItem.id}`);
-
-//   singleTask.appendChild(saveChange);
-
-//   const removeBtn = document.createElement('img');
-//   removeBtn.classList.add(`remove-${editItem.id}`);
-//   removeBtn.setAttribute('src', removeIcon);
-//   singleTask.appendChild(removeBtn);
-
-//   document.querySelectorAll(`.remove-${editItem.id}`).forEach((e) => {
-//     e.addEventListener('click', removeTask);
-//   });
-
-//   document.querySelectorAll(`.save-${editItem.id}`).forEach((e) => {
-//     e.addEventListener('click', saveTask);
-//   });
-// }
-
-// const removeTask = () => {
-
-// };
-
-// const saveTask = () => {
-
-// };
 
 export { storeTask, displayTask };
