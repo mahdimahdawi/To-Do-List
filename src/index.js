b49c0173948dcf5767822654d4543825e9cc057f
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-08-22 21:43:24 +0430

    final Version

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import './style.css';
 import { ClearCompleted } from '../modules/clearComplete.js';
 import { displayTask } from './list.js';
 
-
-
-// const enterBtn = document.querySelector('.enter');
-// enterBtn.addEventListener('click', storeTask);
-// const formBtn = document.querySelector('form');
-// formBtn.addEventListener('submit', storeTask);
 document.querySelector('.clear-all').addEventListener('click', ClearCompleted);
 window.addEventListener('load', displayTask);
