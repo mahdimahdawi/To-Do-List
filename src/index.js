4e0551fe46c6c096986f416de85bc3ea5708f588
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-08-22 17:20:08 +0430

    Update version

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css';
 import { ClearCompleted } from '../modules/clearComplete.js';
 import { displayTask, storeTask } from './list.js';
 
-document.querySelector('.enter').addEventListener('click', storeTask);
+// document.querySelector('.enter').addEventListener('click', storeTask);
 document.querySelector('form').addEventListener('submit', storeTask);
 document.querySelector('.clear-all').addEventListener('click', ClearCompleted);
 window.addEventListener('load', displayTask);
